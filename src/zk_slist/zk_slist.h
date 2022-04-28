e0ac9d0a0b8f8304a09cef5de4e704f56f7bc491
Jose Moreira <joseigorcfm@gmail.com>
2022-04-28 21:54:29 +0200

    add: single linked list functions that still need to be implemented

--- src/zk_slist/zk_slist.h
+++ src/zk_slist/zk_slist.h
@@ -20,8 +20,10 @@ zk_slist_t *zk_slist_copy_deep(const zk_slist_t *list, zk_copy_data_t func, void
 
 zk_slist_t *zk_slist_delete_node(zk_slist_t *list, zk_slist_t *node, zk_destructor_t func);
 
+// FIXME: rename to zk_slist_find
 zk_slist_t *zk_slist_find_by_data(zk_slist_t *list, const void *const data);
 
+// FIXME: rename to zk_slist_find_custom
 zk_slist_t *zk_slist_find_by_data_custom(zk_slist_t *list, const void *const data, zk_compare_t func);
 
 void zk_slist_foreach(zk_slist_t *list, zk_foreach_t func, void *user_data);
@@ -30,10 +32,53 @@ void zk_slist_free(zk_slist_t **list_p);
 
 void zk_slist_free_full(zk_slist_t **list_p, zk_destructor_t destructor);
 
+// FIXME: rename to zk_slist_index
 int zk_slist_get_index(zk_slist_t *list, const void *const data);
 
 zk_slist_t *zk_slist_insert(zk_slist_t *list, void *data, int position);
 
+// TODO: to implement
+zk_slist_t *zk_slist_insert_before(zk_slist_t *list, zk_slist_t *sibling, void *data);
+
+// TODO: to implement
+zk_slist_t *zk_slist_insert_before_link(zk_slist_t *list, zk_slist_t *sibling, zk_slist_t *link);
+
+// TODO: to implement
+zk_slist_t *zk_slist_insert_sorted(zk_slist_t *list, void *data, zk_compare_t func);
+
+// TODO: to implement and create zk_compare_data_t
+zk_slist_t *zk_slist_insert_sorted_with_data(zk_slist_t *list, void *data, zk_compare_t func, void *user_data);
+
 zk_slist_t *zk_slist_last(zk_slist_t *list);
 
+// TODO: to implement
+unsigned int zk_slist_length(zk_slist_t *list);
+
+// TODO: to implement
+zk_slist_t *zk_slist_nth(zk_slist_t *list, unsigned int n);
+
+// TODO: to implement
+void *zk_slist_nth_data(zk_slist_t *list, unsigned int n);
+
+// TODO: to implement
+zk_slist_t *zk_slist_nth_prev(zk_slist_t *list, unsigned int n);
+
+// TODO: to implement
+unsigned int zk_slist_position(zk_slist_t *list, zk_slist_t *link);
+
 zk_slist_t *zk_slist_prepend(zk_slist_t *list, void *data);
+
+// TODO: to implement
+zk_slist_t *zk_slist_remove(zk_slist_t *list, const void *const data);
+
+// TODO: to implement
+zk_slist_t *zk_slist_remove_all(zk_slist_t *list, const void *const data);
+
+// TODO: to implement
+zk_slist_t *zk_slist_reverse(zk_slist_t *list);
+
+// TODO: to implement
+zk_slist_t *zk_slist_sort(zk_slist_t *list, zk_compare_t func);
+
+// TODO: to implement and create zk_compare_data_t
+zk_slist_t *zk_slist_sort_with_data(zk_slist_t *list, zk_compare_t func, void *user_data);
