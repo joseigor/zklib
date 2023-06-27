693eb3e19ee3e24ddca0ba98db2287ace8deea8b
Jose Moreira <joseigorcfm@gmail.com>
2023-06-27 20:19:15 +0200

    refactor: move tests for zk_slist_free to its own file under tests/zk_slist folder

--- tests/zk_slist/test_zk_slist_free.c
+++ tests/zk_slist/test_zk_slist_free.c
@@ -1,25 +1,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "common/test_common.h"
 #include "unity.h"
 #include "zk/zklib.h"
-#include "zk_common/zk_common.h"
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-/*--------------- Test Destructor ---------------*/
-// test zk_free
-void test_zk_free_a_null_list_should_just_return(void)
+void test_zk_slist_free_a_null_list_should_just_return(void)
 {
 	zk_slist *list = NULL;
-	zk_free(&list, NULL);
+	zk_slist_free(&list, NULL);
 	TEST_ASSERT_NULL(list);
 }
 
-void test_zk_free_list_with_no_dynamic_allocated_node_data(void)
+void test_zk_slist_free_list_with_no_dynamic_allocated_node_data(void)
 {
 	zk_slist *list = NULL;
 	int node_1_data = 1;
@@ -32,12 +28,12 @@ void test_zk_free_list_with_no_dynamic_allocated_node_data(void)
 	list = zk_slist_push_back(list, &node_3_data);
 	list = zk_slist_push_back(list, &node_4_data);
 
-	zk_free(&list, NULL);
+	zk_slist_free(&list, NULL);
 
 	TEST_ASSERT_NULL(list);
 }
 
-void test_zk_free_for_list_of_strings(void)
+void test_zk_slist_free_for_list_of_strings(void)
 {
 	int number_of_nodes = 100;
 	zk_slist *list = NULL;
@@ -47,28 +43,23 @@ void test_zk_free_for_list_of_strings(void)
 		TEST_ASSERT_NOT_NULL(list);
 	}
 
-	zk_free(&list, free);
+	zk_slist_free(&list, free);
 	TEST_ASSERT_NULL(list);
 }
 
-void test_zk_free_for_a_null_list_should_just_return(void)
+void test_zk_slist_free_for_a_null_list_should_just_return(void)
 {
 	zk_slist *list = NULL;
-	zk_free(&list, free);
+	zk_slist_free(&list, free);
 	TEST_ASSERT_NULL(list);
 }
 
 int main(void)
 {
 	UNITY_BEGIN();
-
-	// /*--------------- Test Destructor ---------------*/
-
-	{ // tests for zk_free()
-		RUN_TEST(test_zk_free_a_null_list_should_just_return);
-		RUN_TEST(test_zk_free_list_with_no_dynamic_allocated_node_data);
-		RUN_TEST(test_zk_free_for_list_of_strings);
-		RUN_TEST(test_zk_free_for_a_null_list_should_just_return);
-	}
+	RUN_TEST(test_zk_slist_free_a_null_list_should_just_return);
+	RUN_TEST(test_zk_slist_free_list_with_no_dynamic_allocated_node_data);
+	RUN_TEST(test_zk_slist_free_for_list_of_strings);
+	RUN_TEST(test_zk_slist_free_for_a_null_list_should_just_return);
 	return UNITY_END();
 }
