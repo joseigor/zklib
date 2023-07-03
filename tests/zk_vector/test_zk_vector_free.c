3b092c2ce491ab7b55b9953508c151b4e395a89b
Jose Moreira <joseigorcfm@gmail.com>
2023-07-03 20:58:53 +0200

    refactor(tests): add new test to zk_vector_free function
    Added test case test_zk_vector_free_should_free_a_vector_when_a_destructor_function_is_provided

--- tests/zk_vector/test_zk_vector_free.c
+++ tests/zk_vector/test_zk_vector_free.c
@@ -1,5 +1,19 @@
+#include <stdlib.h>
+
 #include "unity.h"
 #include "zk/zklib.h"
+
+typedef struct person {
+	char *name;
+	int age;
+} person;
+
+void person_destructor(void *data)
+{
+	person *p = data;
+	free(p->name);
+	free(p);
+}
 
 void setUp(void) {}
 
@@ -13,7 +27,17 @@ void test_zk_vector_free_should_free_a_vector_when_a_destructor_function_is_not_
 }
 void test_zk_vector_free_should_free_a_vector_when_a_destructor_function_is_provided(void)
 {
-	TEST_IGNORE_MESSAGE("Implement me!");
+	zk_vector *vec = zk_vector_new();
+	// create 100 persons and push them to the vector
+	for (int i = 0; i < 100; i++) {
+		person *p = malloc(sizeof(person));
+		p->name = malloc(10);
+		sprintf(p->name, "person%d", i);
+		p->age = i;
+		zk_vector_push_back(vec, p);
+	}
+
+	zk_vector_free(vec, person_destructor);
 }
 
 int main(void)
